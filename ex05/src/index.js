bd5cbf9c00b18d2c5f393994ffc7c525b3b2dd7f
zabec <denis.imsirovic@gmail.com>
2022-04-06 11:46:49 +0200

    ex05

---
+++ ex05/src/index.js
@@ -0,0 +1,16 @@
+// Only change code below this line
+var school = {
+  name: "Arena",
+  location: "Sarajevo",
+  established: "2020"
+}
+
+function myFunction(name) {
+  school.name = name;
+  return school;
+}
+
+console.log(myFunction("Paragon"));
+
+// Only change code above this line
+module.exports = { school, myFunction };
