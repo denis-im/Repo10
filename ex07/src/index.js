4f3c6e178968cb8d9d50ba9ca0c90e39abc617c4
zabec <denis.imsirovic@gmail.com>
2022-04-06 16:45:43 +0200

    variable moved into func

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,12 +1,12 @@
 // Only change code below this line
-var lion = {
-  name: "Simba",
-  legs: 4,
-  tails: 1,
-  roar: "roar-roar"
-}
-
 function myFunction(prop) {
+  var lion = {
+    name: "Simba",
+    legs: 4,
+    tails: 1,
+    roar: "roar-roar"
+  }
+
   delete lion[prop]
   return lion;
 }
