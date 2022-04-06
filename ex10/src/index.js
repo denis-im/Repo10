1977a308ca7cf22fe612e51bf7121f37d62e939a
zabec <denis.imsirovic@gmail.com>
2022-04-06 13:50:16 +0200

    ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,30 @@
+function myFunction() {
+  var myMusic = {
+    001: {
+      artist: "Billy Joel",
+      title: "Piano Man",
+      release_year: 1973,
+      formats: {
+        1: "CD",
+        2: "8T",
+        3: "LP"
+      },
+      gold: true
+    },
+    002: {
+      artist: "Halid Beslic",
+      title: "Prvi poljubac",
+      release_year: 2003,
+      formats: {
+        1: "CD",
+        2: "8T",
+        3: "LP"
+      }      
+    }
+  }
+  return myMusic;
+}
+
+
+console.log(myFunction()[2]);
+module.exports = myFunction;
