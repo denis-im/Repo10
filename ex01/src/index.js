a9736f97692ee480492dfb00c5153d10d62ae3ae
zabec <denis.imsirovic@gmail.com>
2022-04-06 10:56:56 +0200

    ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+var myPet = {
+  species: "Bulldog",
+  name: "Fido",
+  legs: 4,
+  friends: ["Tom", "Jerry"]
+}
+
+function myFunction(myObj) {
+  return myObj;
+}
+
+// Only change code above this line
+console.log(myFunction(myPet));
+module.exports = { myPet, myFunction };
